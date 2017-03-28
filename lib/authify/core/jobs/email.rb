3a5aa356da41160d109eabb85c8ef3876db296f5
Jonathan Gnagy <jgnagy@knuedge.com>
2017-03-27 17:05:40 -0700

    Fixing typos More version bumping

--- lib/authify/core/jobs/email.rb
+++ lib/authify/core/jobs/email.rb
@@ -4,14 +4,14 @@ module Authify
       # A Resque job for sending email (using Pony)
       class Email
         @queue = :mailer
-        def self.perform(to:, subject:, from: nil, body: nil, html_body: nil, headers: nil)
+        def self.perform(to:, subject:, body: nil, html_body: nil, headers: nil)
           config = {
             to: to,
-            from: ,
+            from: CONFIG[:mail][:from],
             subject: subject,
             via_options: CONFIG[:mail][:server]
           }
-          config[:from]      = from ? from : CONFIG[:mail][:from]
+
           config[:headers]   = headers if headers
           config[:body]      = body if body
           config[:html_body] = html_body if html_body
--- lib/authify/core/version.rb
+++ lib/authify/core/version.rb
@@ -2,8 +2,8 @@ module Authify
   module Core
     VERSION = [
       0, # Major
-      0, # Minor
-      9  # Patch
+      1, # Minor
+      0  # Patch
     ].join('.')
   end
 end
